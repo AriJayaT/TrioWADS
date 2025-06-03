0336c5ce119f1a9c65e3aa720802c01952a0d46a
Ari Jaya Teguh <arijayateguh@gmail.com>
2025-06-03 15:55:02 +0700

    cicd

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         "/": {
           target: mode === "development"
             ? "http://localhost:5000"
-            : "https://e2425-wads-l4acg3-server.csbihub.id",
+            : "https://e2425-wads-l4acg3-server.csbihub.id/api",
           changeOrigin: true,
           secure: mode !== "development",
           ws: true,
