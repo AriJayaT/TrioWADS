e8ca1b480be1a7ad5c6aab4691ed61077b62f4dd
Ari Jaya Teguh <arijayateguh@gmail.com>
2025-06-03 11:25:48 +0700

    cicd

--- server/index.js
+++ server/index.js
@@ -93,12 +93,8 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: process.env.NODE_ENV === 'development'
-          ? `http://localhost:${process.env.PORT || 5000}`
-          : 'https://e2425-wads-l4acg3-server.csbihub.id',
-        description: process.env.NODE_ENV === 'development'
-          ? 'Development server' 
-          : 'Production server'
+        url: "https://e2425-wads-l4acg3-server.csbihub.id/api",
+        description: 'Production server'
       }
     ],
     components: {
