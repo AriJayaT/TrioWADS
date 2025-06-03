fe41a320ea343124c85af70bc7e2a462c4317672
Ari Jaya Teguh <arijayateguh@gmail.com>
2025-06-03 16:57:03 +0700

    cicd

--- server/index.js
+++ server/index.js
@@ -56,7 +56,12 @@ const limiter = rateLimit({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['e2425-wads-l4acg3-client.csbihub.id'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'https://e2425-wads-l4acg3-client.csbihub.id'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
